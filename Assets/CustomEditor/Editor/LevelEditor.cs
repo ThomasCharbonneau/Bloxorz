26a1052f41c3cb734fe1d2b5b4fff0699b18f498
Thomas Charbonneau <tomtom.tc@hotmail.ca>
2021-10-06 16:59:49 -0400

    Add popup to select a type of tile in the level editor

--- Assets/CustomEditor/Editor/LevelEditor.cs
+++ Assets/CustomEditor/Editor/LevelEditor.cs
@@ -2,9 +2,10 @@ using UnityEditor;
 using UnityEngine;
 public class LevelEditor : EditorWindow
 {
-    private const string TILE_INFORMATION_DATABASE_PATH = "../Bloxorz/Assets/Database/TileInformationDatabase.asset";
+    private const string TILE_INFORMATION_DATABASE_PATH = "Assets/Database/TileInformation/TileInformationDatabase.asset";
 
-    private static TileInformationDatabase _tileDatabase;
+    private TileInformationDatabase _tileDatabase;
+    private string[] _tilesName;
 
     private const int MIN_SIZE = 5;
     private const int MAX_SIZE = 20;
@@ -12,16 +13,22 @@ public class LevelEditor : EditorWindow
     private int _nbColumn = 10;
     private int _nbRow = 10;
 
+    private int _tileSelectedIndex = 0;
+
     [MenuItem("Tools/Level Editor")]
     public static void ShowWindow()
     {
-        LoadTileDatabase();
         GetWindow(typeof(LevelEditor));
     }
 
 
     private void OnGUI()
     {
+        if (_tileDatabase == null)
+        {
+            LoadTileDatabase();
+        }
+
         GUILayout.Label("Level Editor", EditorStyles.boldLabel);
         GUILayout.Space(20);
 
@@ -29,6 +36,10 @@ public class LevelEditor : EditorWindow
         int _oldNbRow = _nbRow;
         _nbColumn = EditorGUILayout.IntSlider("Number Of Columns", _nbColumn, MIN_SIZE, MAX_SIZE);
         _nbRow = EditorGUILayout.IntSlider("Number Of Rows", _nbRow, MIN_SIZE, MAX_SIZE);
+
+
+        GUILayout.Space(10);
+        _tileSelectedIndex = EditorGUILayout.Popup("Selected Tile Type", _tileSelectedIndex, _tilesName);
 
         GUILayout.FlexibleSpace();
         if (GUILayout.Button("Save Your Level"))
@@ -52,8 +63,13 @@ public class LevelEditor : EditorWindow
 
     }
 
-    private static void LoadTileDatabase()
+    private void LoadTileDatabase()
     {
         _tileDatabase = AssetDatabase.LoadMainAssetAtPath(TILE_INFORMATION_DATABASE_PATH) as TileInformationDatabase;
+        _tilesName = new string[_tileDatabase.Tiles.Length];
+        for(int i = 0; i < _tileDatabase.Tiles.Length; i++)
+        {
+            _tilesName[i] = _tileDatabase.Tiles[i].name;
+        }
     }
 }
--- Assets/Database/TileInformation/TileInformationDatabase.cs
+++ Assets/Database/TileInformation/TileInformationDatabase.cs
@@ -5,5 +5,5 @@ using UnityEngine;
 [CreateAssetMenu(fileName = "TileInformationDatabase", menuName = "ScriptableObjects/TileInformationDatabase", order = 1)]
 public class TileInformationDatabase : ScriptableObject
 {
-    [SerializeField] private TileInformation[] _tiles = default;
+    [SerializeField] public TileInformation[] Tiles = default;
 }
